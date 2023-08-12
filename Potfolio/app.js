b9a761471cda627912c440dd6271e32b85461db6
CoderNishant202 <142081542+CoderNishant202@users.noreply.github.com>
2023-08-12 15:23:33 +0530

    Delete Potfolio directory

--- Potfolio/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || '8000'
-app.use(express.static('website'))
-app.listen(port,()=>{
-    console.log(`Server is running at https://localhost:${port}`)
-})
